ec89cc41236b3dcb4c8e85763ff25f2a03228422
lihipaniri <lihi.paniri@gmail.com>
2025-06-23 17:43:46 +0300

    [FIXED] - typing

--- cypress/e2e/hero.create.cy.ts
+++ cypress/e2e/hero.create.cy.ts
@@ -5,10 +5,10 @@ import { v4 as uuid } from 'uuid';
 const heroStub = {
   id: Math.floor(Math.random() * 10_000),
   name: `Hero ${uuid().slice(0, 6)}`,
-  suit_color: 'green',
-  has_cape: true,
-  is_retired: false,
-  last_mission: null,
+  suitColor: 'green',
+  hasCape: 1,
+  isRetired: false,
+  lastMission: null,
 };
 
 describe('Hero modal & list', () => {
@@ -21,7 +21,7 @@ describe('Hero modal & list', () => {
 
   it('enables Save when form is valid', () => {
     cy.get('[data-cy="input-hero-name"]').type(heroStub.name);
-    cy.get('[data-cy="input-hero-suit-color"]').type(heroStub.suit_color);
+    cy.get('[data-cy="input-hero-suit-color"]').type(heroStub.suitColor);
     cy.get('[data-cy="checkbox-hero-has-cape"]').check();
   });
 
@@ -30,39 +30,42 @@ describe('Hero modal & list', () => {
   });
 
   it('creates a hero and updates UI using intercept stub', () => {
+    cy.intercept(
+        'POST',
+        `${Cypress.env('apiUrl')}/heroes`,
+        (req) => {
+            expect(req.body.name).to.eq(heroStub.name);
+            req.reply({
+                statusCode: 201,
+                body: heroStub,
+            });
+        }).as('saveHero');
+        
+        cy.intercept('GET', `${Cypress.env('apiUrl')}/heroes`, [heroStub]).as('getHeroes');
 
-  cy.intercept(
-    'POST',
-    `${Cypress.env('apiUrl')}/heroes`,
-    (req) => {
-        expect(req.body.name).to.eq(heroStub.name);
-        req.reply({
-            statusCode: 201,
-            body: heroStub,
+        cy.get('[data-cy="input-hero-name"]').type(heroStub.name);
+        cy.get('[data-cy="input-hero-suit-color"]').type(heroStub.suitColor);
+        cy.get('[data-cy="checkbox-hero-has-cape"]').check();
+
+        cy.get('[data-cy="submit-create-hero-button"]').should('be.visible').and('not.be.disabled').click();
+
+        cy.visit('/');
+        cy.wait('@getHeroes'); 
+        
+        cy.get('[data-cy="hero-create-modal"]').should('not.exist');
+        
+        cy.get(`[data-cy="hero-item-${heroStub.id}"]`)
+        .should('contain.text', heroStub.name)
+        .within(() => {
+            cy.get(`[data-cy="hero-suit-color-${heroStub.id}"]`)
+            .should('contain', heroStub.suitColor);
+            if (heroStub.hasCape) {
+                cy.get(`[data-cy="hero-has-cape-${heroStub.id}"]`).should('be.visible');
+                cy.get(`[data-cy="hero-no-cape-${heroStub.id}"]`).should('not.exist');
+            } else {
+                cy.get(`[data-cy="hero-no-cape-${heroStub.id}"]`).should('be.visible');
+                cy.get(`[data-cy="hero-has-cape-${heroStub.id}"]`).should('not.exist');
+            }
         });
-    }).as('saveHero');
-
-    cy.intercept('GET', `${Cypress.env('apiUrl')}/heroes`, [heroStub]).as('getHeroes');
-
-    cy.get('[data-cy="input-hero-name"]').type(heroStub.name);
-    cy.get('[data-cy="input-hero-suit-color"]').type(heroStub.suit_color);
-    cy.get('[data-cy="checkbox-hero-has-cape"]').check();
-
-    cy.get('[data-cy="submit-create-hero-button"]').should('be.visible').and('not.be.disabled').click();
-
-    cy.visit('/');
-    cy.wait('@getHeroes'); 
-    
-    cy.get('[data-cy="hero-create-modal"]').should('not.exist');
-    console.log(heroStub.name);
-    
-    cy.get(`[data-cy="hero-name-${heroStub.id}"]`).should('contain', heroStub.name);        
-
-    cy.get(`[data-cy="hero-item-${heroStub.id}"]`)
-      .should('contain.text', heroStub.name)
-      .within(() => {
-        cy.get(`[data-cy="hero-suit-color-${heroStub.id}"]`)
-          .should('contain', heroStub.suit_color);
-      });
-  });
+    });
 });
--- src/app/models/hero.ts
+++ src/app/models/hero.ts
@@ -1,7 +1,7 @@
 export interface Hero {
   id: number;
   name: string;
-  suit_color: string;
+  suitColor: string;
   hasCape: boolean;
   lastMission: string | null;
   isRetired: boolean;
